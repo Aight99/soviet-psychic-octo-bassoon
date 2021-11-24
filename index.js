316788097fe8894052c4f8be958980e473a8ce04
Александр Ильченко <ilchenko.as@students.dvfu.ru>
2021-11-24 11:56:29 +1000

    NPM штуки

---
+++ index.js
@@ -0,0 +1,37 @@
+const isNumber = require('is-number');
+
+console.log(isNumber(0));
+console.log(isNumber("a"));
+//
+//
+// // let { v4: uuidv4 } = require('uuid');
+// console.log("Бббббббб")
+// // console.log(uuidv4());
+// console.log("Аааааа")
+//
+//
+// import { v4 as uuidv4 } from 'uuid';
+// uuidv4();
+
+// import { stringify as uuidStringify } from 'uuid';
+//
+// const uuidBytes = [
+//     0x6e,
+//     0xc0,
+//     0xbd,
+//     0x7f,
+//     0x11,
+//     0xc0,
+//     0x43,
+//     0xda,
+//     0x97,
+//     0x5e,
+//     0x2a,
+//     0x8a,
+//     0xd9,
+//     0xeb,
+//     0xae,
+//     0x0b,
+// ];
+//
+// console.log(uuidBytes)
