85b23f6558b20a76ad4e59ac20baef401560f24a
Александр Ильченко <ilchenko.as@students.dvfu.ru>
2021-12-06 20:15:45 +1000

    Webpack приколы

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-const isNumber = require('is-number');
+import { camelCase } from "lodash";
 
-console.log(isNumber(0));
-console.log(isNumber("a"));
-//
-//
-// // let { v4: uuidv4 } = require('uuid');
-// console.log("Бббббббб")
-// // console.log(uuidv4());
-// console.log("Аааааа")
-//
-//
-// import { v4 as uuidv4 } from 'uuid';
-// uuidv4();
-
-// import { stringify as uuidStringify } from 'uuid';
-//
-// const uuidBytes = [
-//     0x6e,
-//     0xc0,
-//     0xbd,
-//     0x7f,
-//     0x11,
-//     0xc0,
-//     0x43,
-//     0xda,
-//     0x97,
-//     0x5e,
-//     0x2a,
-//     0x8a,
-//     0xd9,
-//     0xeb,
-//     0xae,
-//     0x0b,
-// ];
-//
-// console.log(uuidBytes)
+console.log(camelCase('hello world'));
